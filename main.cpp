ecef52bac1c94d99fbfcf5f0c898005e9efdad78
Youssef_elsharbatly <youssefelsharbatly@aucegypt.edu>
2021-02-09 23:54:06 +0400

    main file created

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "function1.h"
+#include "function2.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+   cout << "Enter the number of items in a Knapsack:";
+   int n, W;
+   cin >> n;
+   int v[n], w[n];
+   for (int i = 0; i < n; i++) {
+      cout << "Enter value and weight for item " << i << ":";
+      cin >> v[i];
+      cin >> w[i];
+   }
+   cout << "Enter the capacity of knapsack";
+   cin >> W;
+   cout << knapSack(W, w, v, n);
+   return 0;
+}
